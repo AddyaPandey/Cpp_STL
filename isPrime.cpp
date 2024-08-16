74e60536bbb7a26aab53937250f82e16d2e54a88
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-08-16 22:46:00 +0530

    boolFunction

---
+++ isPrime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+bool isPrime(int num)
+{
+    if (num == 1)
+    {
+        return false;
+    }
+    for (int i = 2; i <= num - 1; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+
+    int num;
+    cin >> num;
+    cout<<isPrime(num);
+}
